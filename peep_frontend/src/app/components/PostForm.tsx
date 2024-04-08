e74dbe51a33fe98dca4d1836ae656b77d6413c31
Koniesjay <jkonyeri@gmail.com>
2024-04-08 11:57:55 +0100

    first commit

--- peep_frontend/src/app/components/PostForm.tsx
+++ peep_frontend/src/app/components/PostForm.tsx
@@ -18,63 +18,118 @@ const PostForm: React.FC<IInputProps> = (props) => {
   const postTextField = useRef(null);
   const [postText, setPostText] = useState<string>("");
   const [isSubmit, setIsSubmit] = useState<boolean>(false);
+  const [formImage, setFormImage] = useState("");
+  const [postMedia, setPostMedia] = useState("");
+  const [formVideo, setFormVideo] = useState("");
+  const [formImagePreview, setFormImagePreview] = useState("");
+  const [formVideoPreview, setFormVideoPreview] = useState("");
+
   const wait = (milliseconds: any) => {
     return new Promise((resolve) => {
       setTimeout(resolve, milliseconds);
     });
   };
-  const [formImage, setFormImage] = useState("");
-  const [formVideo, setFormVideo] = useState("");
-  const [formImagePreview, setFormImagePreview] = useState("");
-  const [formVideoPreview, setFormVideoPreview] = useState("");
+
+  const pinFileToIPFS = async (files: any) => {
+    try {
+      let data = new FormData();
+      data.append("file", files);
+      data.append("pinataOptions", '{"cidVersion": 0}');
+      data.append("pinataMetadata", '{"name": "peeps"}');
+      toast.success("Uploading event image to IPFS .....");
+      const res = await axios.post(
+        "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        data,
+        {
+          headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJkMjEwODYwOC01YzRhLTQ2MDQtOTJjMi1jNTkyMjg1ZGViNzYiLCJlbWFpbCI6ImF3aW5yaW40Ymxlc3NAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siaWQiOiJGUkExIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9LHsiaWQiOiJOWUMxIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6IjJjYmE4MzNkYmM1MjQyNjFiNjU4Iiwic2NvcGVkS2V5U2VjcmV0IjoiZGE2ZWMwZDZlNjBmYmI0ZWY5MTdmOTkzMmFjZWEwZGUyNGFjZTU1NDZmYWQyMTNmYThmZTVlY2RhMDI2NDQ0OCIsImlhdCI6MTcxMTkwODAxNX0.3RVKCUnhqQlgvfy9lxmAa1ltR_sLHVhHSZtvNJj7aik`,
+          },
+        }
+      );
+      console.log(res.data);
+      console.log(
+        `View the file here: https://gateway.pinata.cloud/ipfs/${res.data.IpfsHash}`
+      );
+      toast.success("Event Image upload complete");
+      return {
+        uploaded: true,
+        image: `https://gateway.pinata.cloud/ipfs/${res.data.IpfsHash}`,
+      };
+    } catch (error) {
+      console.log(error);
+      return { uploaded: false, image: `` };
+    }
+  };
+
+  const sendPost = async (imgUrl: string = "") => {
+    try {
+      const data = JSON.stringify({
+        pinataOptions: {
+          cidVersion: 0,
+        },
+        pinataMetadata: {
+          name: "PEEPS_POSTS",
+          keyvalues: {
+            addr: `${wallet?.accounts[0]?.address}`,
+            uuid: uuidv4(),
+          },
+        },
+        pinataContent: {
+          post_username: userData?.username,
+          post_content: postText,
+          post_media: imgUrl,
+          post_comments: 0,
+          post_repeeps: 0,
+          post_likes: 0,
+          createdAt: new Date(),
+        },
+      });
+
+      const res = await axios.post(
+        "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJkMjEwODYwOC01YzRhLTQ2MDQtOTJjMi1jNTkyMjg1ZGViNzYiLCJlbWFpbCI6ImF3aW5yaW40Ymxlc3NAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siaWQiOiJGUkExIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9LHsiaWQiOiJOWUMxIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6IjJjYmE4MzNkYmM1MjQyNjFiNjU4Iiwic2NvcGVkS2V5U2VjcmV0IjoiZGE2ZWMwZDZlNjBmYmI0ZWY5MTdmOTkzMmFjZWEwZGUyNGFjZTU1NDZmYWQyMTNmYThmZTVlY2RhMDI2NDQ0OCIsImlhdCI6MTcxMTkwODAxNX0.3RVKCUnhqQlgvfy9lxmAa1ltR_sLHVhHSZtvNJj7aik`,
+          },
+        }
+      );
+      if (res.data.IpfsHash) {
+        toast.success("Post created");
+        setPostText("");
+        setPostMedia("");
+        setFormImagePreview("");
+        setFormVideoPreview("");
+        setIsSubmit(false);
+        await wait(300);
+
+        setRefreshPost(!refreshPost);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Post not created");
+      setIsSubmit(false);
+    }
+  };
 
   const handlePost = async () => {
     setIsSubmit(true);
     // construct the json payload to send to addInput
     if (wallet) {
       try {
-        const data = JSON.stringify({
-          pinataOptions: {
-            cidVersion: 0,
-          },
-          pinataMetadata: {
-            name: "PEEPS_POSTS",
-            keyvalues: {
-              addr: `${wallet?.accounts[0]?.address}`,
-              uuid: uuidv4(),
-            },
-          },
-          pinataContent: {
-            post_username: userData?.username,
-            post_content: postText,
-            post_media: "",
-            post_comments: 0,
-            post_repeeps: 0,
-            post_likes: 0,
-            createdAt: new Date(),
-          },
-        });
-
-        const res = await axios.post(
-          "https://api.pinata.cloud/pinning/pinJSONToIPFS",
-          data,
-          {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJkMjEwODYwOC01YzRhLTQ2MDQtOTJjMi1jNTkyMjg1ZGViNzYiLCJlbWFpbCI6ImF3aW5yaW40Ymxlc3NAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siaWQiOiJGUkExIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9LHsiaWQiOiJOWUMxIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6IjJjYmE4MzNkYmM1MjQyNjFiNjU4Iiwic2NvcGVkS2V5U2VjcmV0IjoiZGE2ZWMwZDZlNjBmYmI0ZWY5MTdmOTkzMmFjZWEwZGUyNGFjZTU1NDZmYWQyMTNmYThmZTVlY2RhMDI2NDQ0OCIsImlhdCI6MTcxMTkwODAxNX0.3RVKCUnhqQlgvfy9lxmAa1ltR_sLHVhHSZtvNJj7aik`,
-            },
+        if (formImage == "") {
+          await sendPost();
+        } else {
+          const imgUploadRes = await pinFileToIPFS(formImage);
+          await wait(600);
+          if (imgUploadRes.uploaded) {
+            setFormImage("");
+            await sendPost(imgUploadRes.uploaded ? imgUploadRes.image : "");
           }
-        );
-        if (res.data.IpfsHash) {
-          toast.success("Post created");
-          setIsSubmit(false);
-          await wait(300);
-          setRefreshPost(!refreshPost);
         }
       } catch (error) {
         console.log(error);
-        toast.error("Post not created");
-        setIsSubmit(false);
       }
     } else {
       toast.error("Error, Can't make post!");
@@ -104,6 +159,13 @@ const PostForm: React.FC<IInputProps> = (props) => {
     setFormVideo("");
     setFormVideoPreview("");
   };
+
+  useEffect(() => {}, [
+    postText,
+    formImage,
+    formImagePreview,
+    formVideoPreview,
+  ]);
 
   return (
     <div className="bg-base-200 rounded-box focus-within:ring-2 focus-within:ring-primary">
