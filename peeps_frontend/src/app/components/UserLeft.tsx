1b2a9eb04f78bc16124b978546b85869c14bf950
MayowaObisesan <mayowaobi74@gmail.com>
2024-06-01 15:17:02 +0100

    UI update

--- peeps_frontend/src/app/components/UserLeft.tsx
+++ peeps_frontend/src/app/components/UserLeft.tsx
@@ -6,7 +6,11 @@ import { useEffect, useState } from "react";
 import { usePeepsContext } from "../context";
 
 import Image from "next/image";
-import { useActiveAccount, useActiveWalletConnectionStatus, useConnect } from "thirdweb/react";
+import {
+  useActiveAccount,
+  useActiveWalletConnectionStatus,
+  useConnect,
+} from "thirdweb/react";
 import { LucideWallet2 } from "lucide-react";
 
 const defaultImage: string =
@@ -16,11 +20,11 @@ export const Avatar = ({ profileImage }: { profileImage: string }) => {
   return (
     <div className="avatar placeholder">
       <div className="w-12 rounded-full ring ring-primary-content ring-offset-base-100 ring-offset-2">
-        {
-          profileImage
-            ? <Image width={30} height={30} src={profileImage} alt="" />
-            : <span></span>
-        }
+        {profileImage ? (
+          <Image width={30} height={30} src={profileImage} alt="" />
+        ) : (
+          <span></span>
+        )}
       </div>
     </div>
   );
@@ -35,8 +39,19 @@ export const NoProfileCard = () => {
         <ProfileForm />
       </div>
     </div>
-  )
-}
+  );
+};
+
+export const NoSignInCard = () => {
+  return (
+    <div className={"card card-compact bg-blue-600/80 my-4"}>
+      <div className="card-body">
+        <div className="card-title">Action Required</div>
+        Sign in to create your Peep.
+      </div>
+    </div>
+  );
+};
 
 export const UserLeft = () => {
   const {
@@ -62,60 +77,81 @@ export const UserLeft = () => {
           Play your favourite games on a web3 decentralized platform.
         </div>
       </div>
-      <section className={`hidden lg:block w-full sticky lg:top-28 lg:w-[60%] mx-auto px-2`}>
+      <section
+        className={`hidden lg:block w-full sticky lg:top-28 lg:w-[60%] mx-auto px-2`}
+      >
         <div
           className={
             "card card-bordered bg-gray-200 dark:bg-base-300/80 p-4 flex flex-row items-center gap-x-4"
           }
         >
-          {
-            walletStatus === "connected" && hasProfile ? (
-              <>
-                <Avatar profileImage={userData?.profilePicture} />
-                <div className="grow">
-                  <h4 className="font-semibold text-sm text-gray-800 dark:text-white">
-                    {userData?.displayName ? userData?.displayName : "Anonymous"}
-                  </h4>
-                  <div className="text-sm text-gray-800 md:text-gray-500 dark:text-white md:dark:text-gray-500 keep-all overflow-x-hidden text-ellipsis">
-                    @{userData?.username ? userData?.username : "Anonymous"}
+          {walletStatus === "connected" && hasProfile ? (
+            <>
+              <Avatar profileImage={userData?.profilePicture} />
+              <div className="grow">
+                <h4 className="font-semibold text-sm text-gray-800 dark:text-white">
+                  {userData?.displayName ? userData?.displayName : "Anonymous"}
+                </h4>
+                <div className="text-sm text-gray-800 md:text-gray-500 dark:text-white md:dark:text-gray-500 keep-all overflow-x-hidden text-ellipsis">
+                  @{userData?.username ? userData?.username : "Anonymous"}
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              {walletStatus === "connecting" ? (
+                <>
+                  <div className="avatar">
+                    <div className="w-12 rounded-full ring ring-primary-content ring-offset-base-100 ring-offset-2">
+                      <div className="skeleton w-12 h-12 rounded-full"></div>
+                    </div>
                   </div>
-                </div>
-              </>
-            ) : (
-              <>
-                <div className="avatar">
-                  <div className="w-12 rounded-full ring ring-primary-content ring-offset-base-100 ring-offset-2">
-                    <div className="skeleton w-12 h-12 rounded-full"></div>
+                  <div className="grow space-y-2">
+                    <h4 className="skeleton w-full h-6 max-w-sm"></h4>
+                    <p className="skeleton w-20 h-4"></p>
                   </div>
-                </div>
-                <div className="grow space-y-2">
-                  <h4 className="skeleton w-full h-6 max-w-sm"></h4>
-                  <p className="skeleton w-20 h-4"></p>
-                </div>
-              </>
-            )
-          }
+                </>
+              ) : (
+                <>
+                  <div className="avatar">
+                    <div className="w-12 rounded-full ring ring-primary-content ring-offset-base-100 ring-offset-2">
+                      <div className="w-12 h-12 rounded-full"></div>
+                    </div>
+                  </div>
+                  <div className="grow space-y-2">
+                    <h4 className="w-full h-6 max-w-sm bg-base-200 rounded-full"></h4>
+                    <p className="w-20 h-4 bg-base-200 rounded-full"></p>
+                  </div>
+                </>
+              )}
+            </>
+          )}
         </div>
-        {
-          walletStatus === "connected" && hasProfile ? (
-            <div className={"card card-compact bg-gray-200 dark:bg-base-300/80 my-1"}>
-              <div className="card-body">
-                <div className="font-bold text-xs">About me</div>
-                <div className={"keep-all overflow-x-hidden text-ellipsis"}>{userData?.bio ? userData?.bio : "***"}</div>
+        {walletStatus === "connected" && hasProfile ? (
+          <div
+            className={"card card-compact bg-gray-200 dark:bg-base-300/80 my-1"}
+          >
+            <div className="card-body">
+              <div className="font-bold text-xs">About me</div>
+              <div className={"keep-all overflow-x-hidden text-ellipsis"}>
+                {userData?.bio ? userData?.bio : "***"}
               </div>
             </div>
-          ) : null
-        }
-        {walletStatus === "connected" && !hasProfile && (
-          <NoProfileCard />
-        )}
+          </div>
+        ) : null}
+        {walletStatus === "connected" && !hasProfile && <NoProfileCard />}
+        {walletStatus === "disconnected" && <NoSignInCard />}
         <div>
           <ul className="lg:menu menu-md lg:py-4 gap-y-2 lg:[&_a]:py-4">
             {walletStatus === "connected" && hasProfile && (
               <li>
                 <Link
-                  href={userData?.wallet === activeAddress ? `/profile/me` : `/profile/${userData?.username}`}
-                  className="space-x-2 bg-gray-100 dark:bg-base-200"
+                  href={
+                    userData?.wallet === activeAddress
+                      ? `/profile/me`
+                      : `/profile/${userData?.username}`
+                  }
+                  className="space-x-2 hover:bg-gray-100 dark:hover:bg-base-200"
                 >
                   <svg
                     className="w-4 h-4"
@@ -146,25 +182,14 @@ export const UserLeft = () => {
                 </Link>
               </li>
             )}
-            {
-              walletStatus === "connected" && <li>
-              <Link href={"/wallet"} className="space-x-2">
-                <LucideWallet2 width={20} height={20} />
-                <span>Wallet</span>
-              </Link>
-            </li>}
-            {/* <li>
-            <Link href={"/settings"} className="space-x-2">
-              <FaGear />
-              <FcSettings />
-              <span>Options</span>
-            </Link>
-          </li> */}
-            {/* <li>
-            <Link href={""} className="space-x-2">
-              Logout
-            </Link>
-          </li> */}
+            {walletStatus === "connected" && (
+              <li>
+                <Link href={"/wallet"} className="space-x-2 hover:bg-gray-100 dark:hover:bg-base-200 active:bg-green-400 dark:active:bg-green-600">
+                  <LucideWallet2 width={20} height={20} />
+                  <span>Wallet</span>
+                </Link>
+              </li>
+            )}
           </ul>
         </div>
       </section>
